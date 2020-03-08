7061e10070e6dca190023b68c5f703cdc64d378a
Samantha K Krall <SKrall@my.gcu.edu>
2020-03-08 15:37:27 -0400

    Add project files.

---
+++ Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> user = new List<UserModel>();
+
+            UserModel user1 = new UserModel("Sam","heya", "slamminsammy@gcu.com", "1233241234");
+            UserModel user2 = new UserModel("Steven", "kitter", "krall@noneya.net", "4563451234");
+
+            user.Add(user1);
+            user.Add(user2);
+
+            return View("Test", user);
+        }
+    }
+}
---
+++ Activity2Part1/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Activity2Part1.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [DisplayName("Name")]
+        [StringLength(20, MinimumLength =4)]
+        [DefaultValue("")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Password")]
+        [StringLength(20, MinimumLength =4)]
+        [DefaultValue("")]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public UserModel(string Name, string Password, string Email, string Phone)
+        {
+            this.Name = Name;
+            this.Password = Password;
+            this.Email = Email;
+            this.Phone = Phone;
+        }
+    }
+}
